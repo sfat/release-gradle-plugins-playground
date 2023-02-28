83aaa81144ff5056569ffcfc06496f898a3908a5
Andrei Sfat <andrei@sfat.me>
2023-02-28 08:57:54 +0200

    feat: add initial skeleton project

---
+++ src/main/java/me/sfat/releasegradlepluginsplayground/ReleaseGradlePluginsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package me.sfat.releasegradlepluginsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseGradlePluginsPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseGradlePluginsPlaygroundApplication.class, args);
+    }
+
+}
